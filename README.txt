765c18254224af9cd492280497ff33c3c651c4b7
paramat <mat.gregory@virginmedia.com>
2013-07-08 00:31:15 +0100

    Version 0.4.2 Lakes with sealed lakebeds

--- README.txt
+++ README.txt
@@ -1,9 +1,9 @@
-Moonrealm 0.4.1 by paramat
+Moonrealm 0.4.2 by paramat
 For latest stable Minetest compatible back to 0.4.6
 Depends default
 Licenses: Code WTFPL, textures CC BY-SA
 Moonstone and moondust textures are recoloured default textures: sand by VanessaE and stone by Perttu Ahola
-Pine sapling and needles textures by Splizard.
+Pine sapling, needles and ice textures by Splizard.
 
 Fly and teleport up to y = 15000, it will generate below you.
 The top 2 chunk layers (160m depth) contain the surface and are lua generated. Underground i have used Pilzadam's nether mod method to quickly turn air to stone, after that lua generates fissure type cave systems. The underground part of this realm is of unlimited depth.
@@ -30,7 +30,7 @@ Moondusts are no longer falling nodes.
 Mese blocks and iron ore.
 Water ice at high altitudes in moondust and glows gently in the moonlight (light source = 1).
 Water ice can be crafted to default:water_source.
-Moonstone brick and moonstone slab.
+Moonstone brick node and slabs.
 Lux crystals are defined as a fuel with a longer burntime than coal.
 Moonstone can be crafted into default:furnace.
 Moonglass cooked from moondust1.
@@ -41,3 +41,57 @@ Hydroponic liquid crafted from old leaves and water ice, slightly more viscous t
 Only moondust5 is suitable as hydroponic bedding, hydroponic liquid 'source' or 'flowing' will saturate neighbouring moondust5 nodes turning them into moonsoil, in this you can plant a space pine sapling, this will only grow when planted in moonsoil beneath either "air" or "moonrealm:air".
 Removing hydroponic liquid will cause moonsoil to dry out and revert to moondust5.
 Space pines are specially bred compact evergreens for oxygen regeneration and general cool vibage.
+
+New stuff in version 0.4.2:
+
+Liquid hydrocarbon lakes, parameters for colour, transparency, surface level y.
+Lakebeds are a few nodes thick and seal the lake from flowing into the fissures ... which is fun but needs to be done carefully because the structure of the fissures creates a giant underground spreading of waterfalls from a single leak.
+
+Crafting
+
+default water source
+I
+I = waterice
+
+luxnode
+CCC
+CCC
+CCC
+C = luxcrystal
+
+airgen
+SIS
+ILI
+SIS
+S = steel ingot
+I = waterice
+L = luxnode
+
+hlsource
+NNN
+NIN
+NNN
+N = moonrealm:needles
+I = waterice
+
+airlock
+S-S
+SLS
+S-S
+S = steel ingot
+L = luxnode
+
+moonstonebrick x 4
+MM
+MM
+M = moonstone
+
+moonstoneslab x 4
+MM
+M = moonstone
+
+default furnace
+MMM
+M-M
+MMM
+M = moonstone
--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
--- moonrealm 0.4.1 by paramat.
+-- moonrealm 0.4.2 by paramat.
 -- Licenses: Code WTFPL. Textures CC BY-SA.
--- Moonstone and most moondust textures are recoloured default textures: sands by VanessaE and stone by Perttu Ahola.
--- Pine sapling and needles textures by Splizard.
+-- Moonstone and some moondust textures are recoloured default textures: sands by VanessaE and stone by Perttu Ahola.
+-- Pine sapling, needles and ice textures by Splizard.
 
 -- Parameters.
 
@@ -9,6 +9,8 @@ local ONGEN = true -- (true / false) -- Enable / disable moonrealm generation.
 
 local YMIN = 14000 -- Approx bottom. Rounded down to chunk boundary.
 local YMAX = 15000 -- Approx surface. Rounded up to chunk boundary.
+local ATMTOP = 16000 -- Exact top of atmosphere nodes (if ATMOS = true).
+
 local OFFCEN = 80 -- 80 -- Offset centre average. Terrain centre average level, relative to base of surface chunks.
 local CENAMP = 24 -- 24 -- Offset centre amplitude. Terrain centre is varied by this.
 local HIGRAD = 48 -- 48 -- Surface generating noise gradient above offcen. Controls depth of upper terrain.
@@ -18,37 +20,41 @@ local LEXP = 2 -- 2 -- Noise offset exponent below offcen.
 
 local DUSAMP = 0.1 -- 0.1 -- Dust depth amplitude.
 local DUSRAN = 0.01 -- 0.01 -- Dust depth randomness.
-local DUSGRAD = 160 -- 160 -- Dust noise gradient.
+local DUSGRAD = 128 -- 128 -- Dust noise gradient.
 
-local ICEALT = 128 -- 128 -- Ice spawns above this altitude, relative to base of surface chunks.
+local LHCLEV = 48 -- 48 -- Liquid hydrocarbon lake level, relative to base of surface chunks.
+local LHCALP = 192 -- 192 -- LHC alpha.
+local LHCRED = 140 -- 140 -- LHC RGB.
+local LHCGRE = 19 -- 19
+local LHCBLU = 0 -- 0
+
+local ICELEV = 128 -- 128 -- Ice spawns above this altitude, relative to base of surface chunks.
 local ICECHA = 1 -- 1 -- Maximum 1/x chance water ice in dust.
 
 local CAVOFF = 0.02 -- 0.02 -- Cave offset. Size of underground caves.
 local CAVLEV = 0 -- 0 -- Caves thin above this level, relative to base of surface chunks.
 local CAVDIS = 96 -- 96 -- Cave thinning distance in nodes.
 
-local LUX = true -- Enable / disable luxore.
 local LUXCHA = 7*7*7 -- 7*7*7 -- Luxore 1/x chance underground.
 local IROCHA = 5*5*5 -- 5*5*5 -- Iron ore 1/x chance.
 local MESCHA = 23*23*23 -- 23*23*23 -- Mese block 1/x chance.
 
-local ATMOS = true -- Enable / disable tinted atmosphere nodes.
-local ATMTOP = 16000 -- Exact top of atmosphere.
+local ATMOS = true -- Enable / disable tinted atmosphere nodes. 
 local ATMALP = 16 -- 16 -- Atmosphere alpha.
 local ATMRED = 255 -- 255 -- Atmosphere RGB.
 local ATMGRE = 148 -- 148
 local ATMBLU = 0 -- 0
 
 local AIRGEN = true -- Enable/disable air spread abm (in case of air leak).
-local AIRINT = 29 -- Air spread abm interval.
-local SOILINT = 31 -- Hydroponics abm interval.
+local AIRINT = 29 -- 29 -- Air spread abm interval.
 
-local PININT = 57 -- Spawn pine abm interval.
-local PINCHA = 2 -- 1/x chance per sapling.
-local PINMIN = 5 -- Needles height minimum.
-local PINMAX = 7 -- Needles height maximum.
+local SOILINT = 31 -- 31 -- Hydroponics abm interval.
+local PININT = 57 -- 57 -- Spawn pine abm interval.
+local PINCHA = 2 -- 2 -- 1/x chance per sapling.
+local PINMIN = 5 -- 5 -- Needles height minimum.
+local PINMAX = 7 -- 7 -- Needles height maximum.
 
-local PROG = true -- Enable/disable processing progress printed to terminal.
+local PROG = true -- Enable/disable chunk generation progress printed to terminal.
 
 -- Perlin noise for terrain.
 local SEEDDIFF1 = 46894686546
@@ -80,8 +86,9 @@ moonrealm = {}
 
 local yminq = (80 * math.floor((YMIN + 32) / 80)) - 32
 local ymaxq = (80 * math.floor((YMAX + 32) / 80)) + 47
+local levlhc = ymaxq - 159 + LHCLEV
 local levcav = ymaxq - 159 + CAVLEV
-local altice = ymaxq - 159 + ICEALT
+local levice = ymaxq - 159 + ICELEV
 
 -- Nodes.
 
@@ -228,15 +235,15 @@ minetest.register_node("moonrealm:hlflowing", {
 		{
 			image="moonrealm_hlflowing_animated.png",
 			backface_culling=false,
-			animation={type="vertical_frames", aspect_w=16, aspect_h=16, length=1.2}
+			animation={type="vertical_frames", aspect_w=16, aspect_h=16, length=2}
 		},
 		{
 			image="moonrealm_hlflowing_animated.png",
 			backface_culling=true,
-			animation={type="vertical_frames", aspect_w=16, aspect_h=16, length=1.2}
+			animation={type="vertical_frames", aspect_w=16, aspect_h=16, length=2}
 		},
 	},
-	alpha = 192,
+	alpha = 224,
 	paramtype = "light",
 	walkable = false,
 	pointable = false,
@@ -246,7 +253,7 @@ minetest.register_node("moonrealm:hlflowing", {
 	liquid_alternative_flowing = "moonrealm:hlflowing",
 	liquid_alternative_source = "moonrealm:hlsource",
 	liquid_viscosity = 1,
-	post_effect_color = {a=192, r=115, g=55, b=24},
+	post_effect_color = {a=224, r=115, g=55, b=24},
 	groups = {water=3, liquid=3, puts_out_fire=1, not_in_creative_inventory=1},
 })
 
@@ -255,7 +262,7 @@ minetest.register_node("moonrealm:hlsource", {
 	inventory_image = minetest.inventorycube("moonrealm_hl.png"),
 	drawtype = "liquid",
 	tiles = {"moonrealm_hl.png"},
-	alpha = 192,
+	alpha = 224,
 	paramtype = "light",
 	walkable = false,
 	pointable = false,
@@ -264,7 +271,7 @@ minetest.register_node("moonrealm:hlsource", {
 	liquid_alternative_flowing = "moonrealm:hlflowing",
 	liquid_alternative_source = "moonrealm:hlsource",
 	liquid_viscosity = 1,
-	post_effect_color = {a=192, r=115, g=55, b=24},
+	post_effect_color = {a=224, r=115, g=55, b=24},
 	groups = {water=3, liquid=3, puts_out_fire=1},
 })
 
@@ -354,6 +361,55 @@ minetest.register_node("moonrealm:moonstoneslab", {
 	},
 	groups = {cracky=3},
 	sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_node("moonrealm:lhcflowing", {
+	description = "Flowing Liquid Hydrocarbons",
+	inventory_image = minetest.inventorycube("moonrealm_lhc.png"),
+	drawtype = "flowingliquid",
+	tiles = {"moonrealm_lhc.png"},
+	special_tiles = {
+		{
+			image="moonrealm_lhcflowing_animated.png",
+			backface_culling=false,
+			animation={type="vertical_frames", aspect_w=16, aspect_h=16, length=2}
+		},
+		{
+			image="moonrealm_lhcflowing_animated.png",
+			backface_culling=true,
+			animation={type="vertical_frames", aspect_w=16, aspect_h=16, length=2}
+		},
+	},
+	alpha = LHCALP,
+	paramtype = "light",
+	walkable = false,
+	pointable = false,
+	diggable = false,
+	buildable_to = true,
+	liquidtype = "flowing",
+	liquid_alternative_flowing = "moonrealm:lhcflowing",
+	liquid_alternative_source = "moonrealm:lhcsource",
+	liquid_viscosity = 0,
+	post_effect_color = {a=LHCALP, r=LHCRED, g=LHCGRE, b=LHCBLU},
+	groups = {water=3, liquid=3, puts_out_fire=1, not_in_creative_inventory=1},
+})
+
+minetest.register_node("moonrealm:lhcsource", {
+	description = "Liquid Hydrocarbon Source",
+	inventory_image = minetest.inventorycube("moonrealm_lhc.png"),
+	drawtype = "liquid",
+	tiles = {"moonrealm_lhc.png"},
+	alpha = LHCALP,
+	paramtype = "light",
+	walkable = false,
+	pointable = false,
+	buildable_to = true,
+	liquidtype = "source",
+	liquid_alternative_flowing = "moonrealm:lhcflowing",
+	liquid_alternative_source = "moonrealm:lhcsource",
+	liquid_viscosity = 0,
+	post_effect_color = {a=LHCALP, r=LHCRED, g=LHCGRE, b=LHCBLU},
+	groups = {water=3, liquid=3, puts_out_fire=1},
 })
 
 -- Item.
@@ -643,9 +699,11 @@ if ONGEN then
 					print ("[moonrealm] Surface "..x - x0.." ("..minp.x.." "..minp.y.." "..minp.z..")")
 				end
 				for z = z0, z1 do -- for each column do
+					local surfy = false
+					local lakebed = false
 					local noise5 = perlin3:get2d({x=x+828,y=z+828})
 					local cenoff = ymaxq - 159 + OFFCEN + noise5 * CENAMP
-					for y = y0, y1 do -- for each node do
+					for y = y1, y0, -1 do -- for each node do
 						if y > cenoff then
 							offset = ((y - cenoff) / HIGRAD) ^ HEXP
 						else
@@ -655,27 +713,33 @@ if ONGEN then
 						local noise4 = perlin4:get3d({x=x,y=y,z=z})
 						local noise1off = (noise1 + noise4) / 2 - offset
 						if noise1off >= 0 then -- if terrain then
+							if not surfy then
+								surfy = y
+								if surfy <= levlhc + 1 then
+									lakebed = true
+								end
+							end
 							local cavprop = (1 - (y - levcav) / CAVDIS)
 							local offcav = CAVOFF * cavprop
 							local noise2 = perlin2:get3d({x=x,y=y,z=z})
-							if math.abs(noise2) - offcav > 0 then -- if no cave then
+							if math.abs(noise2) - offcav > 0 or (lakebed and noise1off < DUSAMP) then -- if no cave or lakebed then
 								local noise3 = perlin3:get3d({x=x,y=y,z=z})
 								local noise3off = noise3 - (y - cenoff) / DUSGRAD
 								local thrsto = noise3off * DUSAMP + math.random() * DUSRAN
-								if noise1off >= thrsto then
+								if noise1off >= thrsto then -- if stone then
 									if noise1off > DUSAMP and math.random(MESCHA) == 2 then
 										env:add_node({x=x,y=y,z=z},{name="default:mese"})
 									elseif noise1off > DUSAMP and math.random(IROCHA) == 2 then
 										env:add_node({x=x,y=y,z=z},{name="moonrealm:ironore"})
-									elseif LUX and noise1off > DUSAMP and cavprop > 0
+									elseif noise1off > DUSAMP and cavprop > 0
 									and math.random(math.floor(LUXCHA / cavprop)) == 2 then
 										env:add_node({x=x,y=y,z=z},{name="moonrealm:luxore"})
 									else
 										env:add_node({x=x,y=y,z=z},{name="moonrealm:moonstone"})
 									end
-								else
+								else -- dust
 									local noise6 = perlin3:get3d({x=x-828,y=y-828,z=z-828}) + math.random() * 0.1
-									if y > altice and math.random(math.floor(ICECHA * (ymaxq - altice) / (y - altice))) == 1 then
+									if y > levice and math.random(math.floor(ICECHA * (ymaxq - levice) / (y - levice))) == 1 then
 										env:add_node({x=x,y=y,z=z},{name="moonrealm:waterice"})
 									elseif noise6 < -0.9 then
 										env:add_node({x=x,y=y,z=z},{name="moonrealm:moondust1"})
@@ -692,6 +756,8 @@ if ONGEN then
 							elseif ATMOS then -- if cave add atmos
 								env:add_node({x=x,y=y,z=z},{name="moonrealm:atmos"})
 							end
+						elseif y <= levlhc then -- if lake then
+							env:add_node({x=x,y=y,z=z},{name="moonrealm:lhcsource"})
 						end
 					end
 				end
